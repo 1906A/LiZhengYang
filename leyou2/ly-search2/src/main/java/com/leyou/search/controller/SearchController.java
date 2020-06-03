cf10403f3242cf8e174e303a0f42f1e58584cbc5
281744236 <281744236@qq.com>
2020-06-03 15:42:13 +0800

    java

--- leyou2/ly-search2/src/main/java/com/leyou/search/controller/SearchController.java
+++ leyou2/ly-search2/src/main/java/com/leyou/search/controller/SearchController.java
@@ -3,16 +3,20 @@ package com.leyou.search.controller;
 import com.leyou.common.PageResult;
 import com.leyou.pojo.Brand;
 import com.leyou.pojo.Category;
+import com.leyou.pojo.SpecParam;
 import com.leyou.search.client.BrandClient;
 import com.leyou.search.client.CategoryClient;
+import com.leyou.search.client.SpecClient;
 import com.leyou.search.item.Goods;
 import com.leyou.search.pojo.SearchRequest;
 import com.leyou.search.pojo.SearchResult;
 import com.leyou.search.repository.GoodsRepository;
 import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.LongTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +28,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("search")
@@ -36,6 +42,8 @@ public class SearchController {
     CategoryClient categoryClient;
     @Autowired
     BrandClient brandClient;
+    @Autowired
+    SpecClient specClient;
 
     @RequestMapping("page")
     public PageResult<Goods> page(@RequestBody SearchRequest searchRequest){
@@ -76,9 +84,41 @@ public class SearchController {
             brandList.add(brand);
         });
 
+        List<Map<String,Object>> paramList = new ArrayList<>();
+        if (categoryList.size()==1){
+            List<SpecParam> specParams = specClient.findSpecParamByCidAndSearching(categoryList.get(0).getId());
+            specParams.forEach(specParam -> {
+                String key = specParam.getName();
+                builder.addAggregation(AggregationBuilders.terms(key).field("specs."+key+".keyword"));
+            });
+
+            AggregatedPage<Goods> search1 = (AggregatedPage<Goods>)goodsRepository.search(builder.build());
+            Map<String, Aggregation> aggregationMap = search1.getAggregations().asMap();
+
+            aggregationMap.keySet().forEach(mKey ->{
+                if (!(mKey.equals(categoryName)||mKey.equals(brandName))){
+                    StringTerms aggregation = (StringTerms)aggregationMap.get(mKey);
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("key",mKey);
+                    List<Map<String,String>> list = new ArrayList<>();
+
+                    aggregation.getBuckets().forEach(bucket -> {
+                        Map<String,String> valueMap = new HashMap<>();
+                        valueMap.put("name",bucket.getKeyAsString());
+                        list.add(valueMap);
+                    });
+
+                    map.put("options",list);
+                    paramList.add(map);
+                }
+            });
+        }
+
+
+
         //Page<Goods> search = goodsRepository.search(builder.build());
 
         return new SearchResult(search.getTotalElements(), search.getContent(),
-                search.getTotalPages(),categoryList,brandList);
+                search.getTotalPages(),categoryList,brandList,paramList);
     }
 }
--- leyou2/ly-search2/src/main/java/com/leyou/search/pojo/SearchResult.java
+++ leyou2/ly-search2/src/main/java/com/leyou/search/pojo/SearchResult.java
@@ -6,16 +6,22 @@ import com.leyou.pojo.Category;
 import com.leyou.search.item.Goods;
 
 import java.util.List;
+import java.util.Map;
 
 public class SearchResult extends PageResult<Goods> {
 
     private List<Category> categoryList;
     private List<Brand> brandList;
 
-    public SearchResult(Long total, List<Goods> items, Integer totalPage, List<Category> categoryList, List<Brand> brandList) {
+    private List<Map<String,Object>> paramList;
+
+
+    public SearchResult(Long total, List<Goods> items, Integer totalPage, List<Category> categoryList,
+                        List<Brand> brandList, List<Map<String, Object>> paramList) {
         super(total, items, totalPage);
         this.categoryList = categoryList;
         this.brandList = brandList;
+        this.paramList = paramList;
     }
 
     public SearchResult(Long total) {
@@ -44,5 +50,13 @@ public class SearchResult extends PageResult<Goods> {
 
     public void setBrandList(List<Brand> brandList) {
         this.brandList = brandList;
+    }
+
+    public List<Map<String, Object>> getParamList() {
+        return paramList;
+    }
+
+    public void setParamList(List<Map<String, Object>> paramList) {
+        this.paramList = paramList;
     }
 }
